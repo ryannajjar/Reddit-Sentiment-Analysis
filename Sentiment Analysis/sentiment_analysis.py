b7bc28b35379195353d933a715449aef9ed36534
Ryan Najjar <ryannajjar@Ryans-MacBook-Air.local>
2022-10-04 13:10:28 -0500

    add body method to RedditSA class that runs sentiment analysis on text in body of Reddit posts

--- Sentiment Analysis/sentiment_analysis.py
+++ Sentiment Analysis/sentiment_analysis.py
@@ -17,13 +17,24 @@ nlp.add_pipe('spacytextblob')
 
 
 class SubredditSA:
-    """Runs sentiment analysis on a specified amount of Reddit posts under hot, new, top, or rising in a specific subreddit."""
+    """Runs sentiment analysis on aspects of Reddit posts in a specific subreddit."""
     def __init__(self, subreddit):
         self.subreddit = subreddit
     
+    """Runs sentiment analysis on a specified amount of Reddit posts' titles under hot, new, top, or rising."""
     def title(self, post_relevance, num_posts):
         for submission in eval(f'reddit.subreddit("{self.subreddit}").{post_relevance}(limit={num_posts})'):
             print(submission.title)
-            post_doc = nlp(submission.title)
-            print(post_doc._.blob.polarity)
-            print()
+            print('\n' * 2)
+            posttitle_doc = nlp(submission.title)
+            print(f'The title of this post has a sentiment of: {posttitle_doc._.blob.polarity}')
+            print('*' * 100 + '\n' * 2 + '*' * 100)
+
+    """Runs sentiment analysis on a specified amount of Reddit posts' body under hot, new, top, or rising."""
+    def body(self, post_relevance, num_posts): # method is not complete, fix so that it includes images
+        for submission in eval(f'reddit.subreddit("{self.subreddit}").{post_relevance}(limit={num_posts})'):
+            print(submission.selftext)
+            print('\n' * 2)
+            postbody_doc = nlp(submission.title)
+            print(f'The body of this post has a sentiment of: {postbody_doc._.blob.polarity}')
+            print('*' * 100 + '\n' * 2 + '*' * 100)
