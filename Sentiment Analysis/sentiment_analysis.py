c918f5f543c98f52730573634557b74f71022bc9
Ryan Najjar <ryannajjar7@gmail.com>
2022-11-11 08:44:58 -0600

    add top_comments method that runs sentiment analysis on top comments of post, and averages sentiment values

--- Sentiment Analysis/sentiment_analysis.py
+++ Sentiment Analysis/sentiment_analysis.py
@@ -37,10 +37,45 @@ class SubredditSA:
     """Runs sentiment analysis on a specified amount of Reddit posts' body under hot, new, top, or rising."""
     def body(self, post_relevance, num_posts): # method is not complete, fix so that it includes images
         for submission in eval(f'reddit.subreddit("{self.subreddit}").{post_relevance}(limit={num_posts})'):
-            print(f'Title: {submission.title}')
-            print('\n' * 2 + '*' * 100 + '\n' * 2)
+            print(f'Title of the post: {submission.title}')
+            print('\n' * 2 + '[]' * 50 + '\n' * 2)
             print(submission.selftext)
             print('\n' * 2)
             postbody_doc = nlp(submission.selftext)
             print(f'The body of this post has a sentiment of: {postbody_doc._.blob.polarity}')
             print('*' * 100 + '\n' * 2 + '*' * 100 + '\n')
+
+    """Runs sentiment analysis on the top comments of a reddit post, and averages the values to get a total idea of the sentiment."""
+    def top_comments(self, post_relevance, num_posts):
+        sentiment_values = []
+        total_sentiment = 0
+
+        for submission in eval(f'reddit.subreddit("{self.subreddit}").{post_relevance}(limit={num_posts})'):
+            print(f'Title of the post: {submission.title}')
+            print('\n' * 2 + '[]' * 50 + '\n' * 2)
+            for top_level_comment in submission.comments:
+                if top_level_comment.body == '[deleted]':
+                    print(top_level_comment.body)
+                    print('UNABLE TO RUN SENTIMENT ANALYSIS, COMMENT WAS DELETED')
+                    print('\n' + '*' * 100 + '\n')
+                elif top_level_comment.body == '[removed]':
+                    print(top_level_comment.body)
+                    print('UNABLE TO RUN SENTIMENT ANALYSIS, COMMENT WAS REMOVED')
+                    print('\n' + '*' * 100 + '\n')
+                else:
+                    print(top_level_comment.body)
+                    posttitle_doc = nlp(top_level_comment.body)
+                    print(f'This comment has a sentiment of: {posttitle_doc._.blob.polarity}')
+                    sentiment_values.append(posttitle_doc._.blob.polarity)
+                    print('\n' + '*' * 100 + '\n')
+
+            for sentiment in sentiment_values:
+                total_sentiment += sentiment
+            total_sentiment /= len(sentiment_values)
+            print('\n' * 2 + '[]' * 50 + '\n' * 2)
+            print(f'The sentiment of the people is: {total_sentiment}')
+            print('\n' * 2 + '[]' * 50 + '\n' * 2)
+
+
+test = SubredditSA('chess')
+test.top_comments('hot', 2)
