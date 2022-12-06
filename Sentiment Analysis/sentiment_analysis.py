79db25266fdd4cb4a3a43537a586570b32bb0eb3
Ryan Najjar <ryannajjar7@gmail.com>
2022-12-05 22:57:57 -0600

    write sub_comments sentiment analysis method

--- Sentiment Analysis/sentiment_analysis.py
+++ Sentiment Analysis/sentiment_analysis.py
@@ -4,6 +4,8 @@ from creds import *
 import spacy
 from spacytextblob.spacytextblob import SpacyTextBlob
 
+from collections import deque
+
 
 reddit = praw.Reddit(
     client_id=CLIENT_ID,
@@ -37,6 +39,7 @@ class SubredditSA:
         for submission in eval(f'reddit.subreddit("{self.subreddit}").{post_relevance}(limit={num_posts})'):
             print(f'Title of the post: {submission.title}')
             print('\n' * 2 + '[]' * 50 + '\n' * 2)
+
             print(submission.selftext)
             print('\n' * 2)
             postbody_doc = nlp(submission.selftext)
@@ -45,12 +48,13 @@ class SubredditSA:
 
     """Runs sentiment analysis on the top comments of a reddit post, and averages the values to get a total idea of the sentiment."""
     def top_comments(self, post_relevance, num_posts):
-        avg_sentiment = 0
-        comments_analyzed = 0
-        
         for submission in eval(f'reddit.subreddit("{self.subreddit}").{post_relevance}(limit={num_posts})'):
+            avg_sentiment = 0
+            comments_analyzed = 0
+
             print(f'Title of the post: {submission.title}')
             print('\n' * 2 + '[]' * 50 + '\n' * 2)
+
             for top_level_comment in submission.comments:
                 if top_level_comment.body == '[deleted]':
                     print(top_level_comment.body)
@@ -72,3 +76,51 @@ class SubredditSA:
             print('\n' * 2 + '[]' * 50 + '\n' * 2)
             print(f'The sentiment of the people is: {avg_sentiment}')
             print('\n' * 2 + '[]' * 50 + '\n' * 2)
+    
+    """Runs sentiment analysis on the sub comments of a reddit post, and averages the values to get a total idea of the sentiment."""
+    def sub_comments(self, post_relevance, num_posts, level=2):
+        for submission in eval(f'reddit.subreddit("{self.subreddit}").{post_relevance}(limit={num_posts})'):
+            avg_sentiment = 0
+            comment_replies_analyzed = 0
+
+            print(f'Title of the post: {submission.title}')
+            print('\n' * 2 + '[]' * 50 + '\n' * 2)
+
+            comment_replies_in_level = []
+            for comment in submission.comments:
+                queue = deque([comment])
+                visited = {comment}
+                depth_counter = 1
+
+                while len(queue) > 0:
+                    for _ in range(len(queue)):
+                        comment = queue.popleft()                            
+
+                        for comment_reply in comment.replies:
+                            if comment_reply not in visited:
+                                visited.add(comment_reply)
+                                queue.append(comment_reply)
+
+                    depth_counter += 1
+                    if depth_counter == level:
+                        break
+
+                comment_replies_in_level += queue
+                    
+            # Process comment replies in desired level
+            for reply in comment_replies_in_level:
+                print(reply.body)
+                posttitle_doc = nlp(reply.body)
+                print(f'This comment reply has a sentiment of: {posttitle_doc._.blob.polarity}')
+                avg_sentiment += posttitle_doc._.blob.polarity
+                comment_replies_analyzed += 1
+                print('\n' + '*' * 100 + '\n')
+
+            avg_sentiment /= comment_replies_analyzed
+            print('\n' * 2 + '[]' * 50 + '\n' * 2)
+            print(f'The sentiment of the people is: {avg_sentiment}')
+            print('\n' * 2 + '[]' * 50 + '\n' * 2)
+
+
+test = SubredditSA('chess')
+test.sub_comments('hot', 1, 3)
