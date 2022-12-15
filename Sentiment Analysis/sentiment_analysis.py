5e81ac99f2621a055bd3f169a7b4f0deac7bfd0b
Ryan Najjar <ryannajjar7@gmail.com>
2022-12-15 12:42:21 -0600

    move output of all methods from STDOUT to external files

--- Sentiment Analysis/sentiment_analysis.py
+++ Sentiment Analysis/sentiment_analysis.py
@@ -30,60 +30,72 @@ class SubredditSA:
     def title(self, post_relevance, num_posts):
         """Runs sentiment analysis on a specified amount of Reddit posts' titles under hot, new, top, or rising."""
 
+        f = open('title_data.txt', 'w')
+
         for submission in eval(f'reddit.subreddit("{self.subreddit}").{post_relevance}(limit={num_posts})'):
-            print(submission.title)
-            print('\n' * 2)
+            f.write(submission.title)
+            f.write('\n' * 2)
             posttitle_doc = nlp(submission.title)
-            print(f'The title of this post has a sentiment of: {posttitle_doc._.blob.polarity}')
-            print('*' * 100 + '\n' * 2 + '*' * 100 + '\n')
+            f.write(f'The title of this post has a sentiment of: {posttitle_doc._.blob.polarity}')
+            f.write('*' * 100 + '\n' * 2 + '*' * 100 + '\n')
+
+        f.close()
 
     def body(self, post_relevance, num_posts): # method is not complete, change after to include images after finishing class
         """Runs sentiment analysis on a specified amount of Reddit posts' body under hot, new, top, or rising."""
 
-        for submission in eval(f'reddit.subreddit("{self.subreddit}").{post_relevance}(limit={num_posts})'):
-            print(f'Title of the post: {submission.title}')
-            print('\n' * 2 + '[]' * 50 + '\n' * 2)
+        f = open('body_data.txt', 'w')
 
-            print(submission.selftext)
-            print('\n' * 2)
+        for submission in eval(f'reddit.subreddit("{self.subreddit}").{post_relevance}(limit={num_posts})'):
+            f.write(f'Title of the post: {submission.title}')
+            f.write('\n' * 2 + '[]' * 50 + '\n' * 2)
+
+            f.write(submission.selftext)
+            f.write('\n' * 2)
             postbody_doc = nlp(submission.selftext)
-            print(f'The body of this post has a sentiment of: {postbody_doc._.blob.polarity}')
-            print('*' * 100 + '\n' * 2 + '*' * 100 + '\n')
+            f.write(f'The body of this post has a sentiment of: {postbody_doc._.blob.polarity}')
+            f.write('*' * 100 + '\n' * 2 + '*' * 100 + '\n')
+
+        f.close()
 
     def top_comments(self, post_relevance, num_posts):
         """Runs sentiment analysis on the top comments of a reddit post, and averages the values to get a total idea of the sentiment."""
+
+        f = open('comment__data.txt', 'w')
 
         for submission in eval(f'reddit.subreddit("{self.subreddit}").{post_relevance}(limit={num_posts})'):
             avg_sentiment = 0
             comments_analyzed = 0
 
-            print(f'Title of the post: {submission.title}')
-            print('\n' * 2 + '[]' * 50 + '\n' * 2)
+            f.write(f'Title of the post: {submission.title}')
+            f.write('\n' * 2 + '[]' * 50 + '\n' * 2)
 
             for top_level_comment in self._only_comments(submission.comments):
                 if top_level_comment.body == '[deleted]':
-                    print(top_level_comment.body)
-                    print('UNABLE TO RUN SENTIMENT ANALYSIS, COMMENT WAS DELETED')
-                    print('\n' + '*' * 100 + '\n')
+                    f.write(top_level_comment.body)
+                    f.write('UNABLE TO RUN SENTIMENT ANALYSIS, COMMENT WAS DELETED')
+                    f.write('\n' + '*' * 100 + '\n')
                 elif top_level_comment.body == '[removed]':
-                    print(top_level_comment.body)
-                    print('UNABLE TO RUN SENTIMENT ANALYSIS, COMMENT WAS REMOVED')
-                    print('\n' + '*' * 100 + '\n')
+                    f.write(top_level_comment.body)
+                    f.write('UNABLE TO RUN SENTIMENT ANALYSIS, COMMENT WAS REMOVED')
+                    f.write('\n' + '*' * 100 + '\n')
                 else:
-                    print(top_level_comment.body)
+                    f.write(top_level_comment.body)
                     postcomment_doc = nlp(top_level_comment.body)
-                    print(f'This comment has a sentiment of: {postcomment_doc._.blob.polarity}')
+                    f.write(f'This comment has a sentiment of: {postcomment_doc._.blob.polarity}')
                     avg_sentiment += postcomment_doc._.blob.polarity
                     comments_analyzed += 1
-                    print('\n' + '*' * 100 + '\n')
+                    f.write('\n' + '*' * 100 + '\n')
 
             if comments_analyzed == 0:
-                print('There are no comments to analyze in this Reddit post.')
+                f.write('There are no comments to analyze in this Reddit post.')
             else:
                 avg_sentiment /= comments_analyzed
-                print('\n' * 2 + '[]' * 50 + '\n' * 2)
-                print(f'The sentiment of the people is: {avg_sentiment}')
-                print('\n' * 2 + '[]' * 50 + '\n' * 2)
+                f.write('\n' * 2 + '[]' * 50 + '\n' * 2)
+                f.write(f'The sentiment of the people is: {avg_sentiment}')
+                f.write('\n' * 2 + '[]' * 50 + '\n' * 2)
+
+        f.close()
     
     def sub_comments(self, post_relevance, num_posts, level=2):
         """Runs sentiment analysis on the sub comments of a reddit post, and averages the values to get a total idea of the sentiment."""
