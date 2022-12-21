514d29213a85805d7a7e439050e86643602ef73c
Ryan Najjar <ryannajjar7@gmail.com>
2022-12-22 00:17:54 +0200

    add formatting.py file with functions for formatting output

--- Sentiment Analysis/sentiment_analysis.py
+++ Sentiment Analysis/sentiment_analysis.py
@@ -1,11 +1,16 @@
+# Standard
+from collections import deque
+
+# In-project
+import formatting as fm
+
+# Third-party
 import praw
 from creds import *
 from praw.models import MoreComments
 
 import spacy
 from spacytextblob.spacytextblob import SpacyTextBlob
-
-from collections import deque
 
 
 reddit = praw.Reddit(
@@ -22,8 +27,10 @@ nlp.add_pipe('spacytextblob')
 
 
 class SubredditSA:
+    """Runs sentiment analysis on aspects of Reddit posts in a specific subreddit."""
+
     def __init__(self, subreddit):
-        """Runs sentiment analysis on aspects of Reddit posts in a specific subreddit."""
+        """Initializes the subreddit that data will be pulled from."""
 
         self.subreddit = subreddit
     
@@ -37,7 +44,7 @@ class SubredditSA:
             f.write('\n')
             posttitle_doc = nlp(submission.title)
             f.write(f'The title of this post has a sentiment of: {posttitle_doc._.blob.polarity}\n')
-            f.write('*' * 100 + '\n' * 2 + '*' * 100 + '\n')
+            f.write(fm.mini_separator_1())
 
         f.close()
 
@@ -47,15 +54,15 @@ class SubredditSA:
         f = open('body_data.txt', 'w')
 
         for submission in eval(f'reddit.subreddit("{self.subreddit}").{post_relevance}(limit={num_posts})'):
-            f.write('\n' + '[]' * 50 + '\n' * 2)
-            f.write(f'Title of the post: {submission.title}\n')
-            f.write('\n' + '[]' * 50 + '\n' * 2)
+            f.write(fm.big_separator_1())
+            f.write(fm.display_title(submission.title))
+            f.write(fm.big_separator_1())
 
             f.write(submission.selftext + '\n')
             f.write('\n')
             postbody_doc = nlp(submission.selftext)
             f.write(f'The body of this post has a sentiment of: {postbody_doc._.blob.polarity}\n')
-            f.write('*' * 100 + '\n' * 2 + '*' * 100 + '\n')
+            f.write(fm.mini_separator_1())
 
         f.close()
 
@@ -68,33 +75,33 @@ class SubredditSA:
             avg_sentiment = 0
             comments_analyzed = 0
 
-            f.write('\n' + '[]' * 50 + '\n' * 2)
-            f.write(f'Title of the post: {submission.title}\n')
-            f.write('\n' + '[]' * 50 + '\n' * 2)
+            f.write(fm.big_separator_1())
+            f.write(fm.display_title(submission.title))
+            f.write(fm.big_separator_1())
 
             for top_level_comment in self._only_comments(submission.comments):
                 if top_level_comment.body == '[deleted]':
                     f.write(top_level_comment.body + '\n')
                     f.write('UNABLE TO RUN SENTIMENT ANALYSIS, COMMENT WAS DELETED\n')
-                    f.write('\n' + '*' * 100 + '\n')
+                    f.write(fm.mini_separator_2())
                 elif top_level_comment.body == '[removed]':
                     f.write(top_level_comment.body + '\n')
                     f.write('UNABLE TO RUN SENTIMENT ANALYSIS, COMMENT WAS REMOVED\n')
-                    f.write('\n' + '*' * 100 + '\n')
+                    f.write(fm.mini_separator_2())
                 else:
                     f.write(top_level_comment.body + '\n')
                     postcomment_doc = nlp(top_level_comment.body)
                     f.write(f'\n\nThis comment has a sentiment of: {postcomment_doc._.blob.polarity}\n')
                     avg_sentiment += postcomment_doc._.blob.polarity
                     comments_analyzed += 1
-                    f.write('\n' + '*' * 100 + '\n')
+                    f.write(fm.mini_separator_2())
 
             if comments_analyzed == 0:
                 f.write('There are no comments to analyze in this Reddit post.\n')
             else:
                 avg_sentiment /= comments_analyzed
-                f.write('\n' * 2 + '[]' * 50 + '\n' * 2)
-                f.write(f'The sentiment of the people is: {avg_sentiment}\n')
+                f.write(fm.big_separator_2())
+                f.write(fm.display_average_sentiment(avg_sentiment))
 
         f.close()
     
@@ -107,9 +114,9 @@ class SubredditSA:
             avg_sentiment = 0
             comment_replies_analyzed = 0
 
-            f.write('\n' + '[]' * 50 + '\n' * 2)
-            f.write(f'Title of the post: {submission.title}\n')
-            f.write('\n' + '[]' * 50 + '\n' * 2)
+            f.write(fm.big_separator_1())
+            f.write(fm.display_title(submission.title))
+            f.write(fm.big_separator_1())
 
             parents = []
             for comment in self._only_comments(submission.comments):
@@ -139,29 +146,29 @@ class SubredditSA:
                 f.write('\n')
                 for comment_reply in self._only_comments(parent_comment.replies):
                     if comment_reply.body == '[deleted]':
-                        f.write('\t' + comment_reply.body.replace('\n', '\n\t') + '\n')
+                        f.write(fm.indent(comment_reply.body))
                         f.write('\tUNABLE TO RUN SENTIMENT ANALYSIS, COMMENT WAS DELETED\n')
-                        f.write('\n\t' + '*' * 100 + '\n')
+                        f.write(fm.mini_separator_2(True))
                     elif comment_reply.body == '[removed]':
-                        f.write('\t' + comment_reply.body.replace('\n', '\n\t') + '\n')
+                        f.write(fm.indent(comment_reply.body))
                         f.write('\tUNABLE TO RUN SENTIMENT ANALYSIS, COMMENT WAS REMOVED\n')
-                        f.write('\n\t' + '*' * 100 + '\n')
+                        f.write(fm.mini_separator_2(True))
                     else:
-                        f.write('\t' + comment_reply.body.replace('\n', '\n\t') + '\n')
+                        f.write(fm.indent(comment_reply.body))
                         postcommentreply_doc = nlp(comment_reply.body)
                         sentiment_value = postcommentreply_doc._.blob.polarity / 3
                         f.write(f'\n\n\tThis comment reply has a sentiment of: {sentiment_value}\n')
                         avg_sentiment += sentiment_value
                         comment_replies_analyzed += 1
-                        f.write('\n\t' + '*' * 100 + '\n')
-                f.write('*' * 100 + '\n')
+                        f.write(fm.mini_separator_2(True))
+                f.write(fm.mini_separator_3())
 
             if comment_replies_analyzed == 0:
                 f.write(f'There are no sub comments in the Reddit post on level {level} to analyze.\n')
             else:
                 avg_sentiment /= comment_replies_analyzed
-                f.write('\n' * 2 + '[]' * 50 + '\n' * 2)
-                f.write(f'The sentiment of the people is: {avg_sentiment}\n')
+                f.write(fm.big_separator_2())
+                f.write(fm.display_average_sentiment(avg_sentiment))
             
         f.close()
     
@@ -171,31 +178,31 @@ class SubredditSA:
         f = open('votes_data.txt', 'w')
 
         for submission in eval(f'reddit.subreddit("{self.subreddit}").{post_relevance}(limit={num_posts})'):
-            f.write('\n' + '[]' * 50 + '\n' * 2)
-            f.write(f'Title of the post: {submission.title}\n')
-            f.write('\n' + '[]' * 50 + '\n' * 2)
+            f.write(fm.big_separator_1())
+            f.write(fm.display_title(submission.title))
+            f.write(fm.big_separator_1())
 
             if post_relevance == 'new':
                 if submission.score == 0 and submission.upvote_ratio == 0:
-                    f.write(f'{submission.upvote_ratio * 100}% of the votes on this post are upvotes.\n')
+                    f.write(fm.display_upvote_percentage(submission.upvote_ratio * 100))
                     f.write('\n')
-                    f.write('Based on upvote ratio, the sentiment of the people is: 0.0\n')
-                    f.write('*' * 100 + '\n' * 2 + '*' * 100 + '\n')
+                    f.write(fm.display_upvote_ratio_sentiment(0.0))
+                    f.write(fm.mini_separator_1())
                 elif 0 <= submission.score < 100 or submission.socre < 0:
-                    f.write(f'{submission.upvote_ratio * 100}% of the votes on this post are upvotes.\n')
+                    f.write(fm.display_upvote_percentage(submission.upvote_ratio * 100))
                     f.write('\n')
-                    f.write(f'Based on upvote ratio, the sentiment of the people is: {(2 * submission.upvote_ratio - 1) / 3}\n')
-                    f.write('*' * 100 + '\n' * 2 + '*' * 100 + '\n')
+                    f.write(fm.display_upvote_ratio_sentiment(self._upvote_ratio_to_sentiment_value(submission.upvote_ratio / 3)))
+                    f.write(fm.mini_separator_1())
                 elif submission.score >= 100:
-                    f.write(f'{submission.upvote_ratio * 100}% of the votes on this post are upvotes.\n')
+                    f.write(fm.display_upvote_percentage(submission.upvote_ratio * 100))
                     f.write('\n')
-                    f.write(f'Based on upvote ratio, the sentiment of the people is: {2 * submission.upvote_ratio - 1}\n')
-                    f.write('*' * 100 + '\n' * 2 + '*' * 100 + '\n')
+                    f.write(fm.display_upvote_ratio_sentiment(self._upvote_ratio_to_sentiment_value(submission.upvote_ratio)))
+                    f.write(fm.mini_separator_1())
 
-            f.write(f'{submission.upvote_ratio * 100}% of the votes on this post are upvotes.\n')
+            f.write(fm.display_upvote_percentage(submission.upvote_ratio * 100))
             f.write('\n')
-            f.write(f'Based on upvote ratio, the sentiment of the people is: {2 * submission.upvote_ratio - 1}\n')
-            f.write('*' * 100 + '\n' * 2 + '*' * 100 + '\n')
+            f.write(fm.display_upvote_ratio_sentiment(self._upvote_ratio_to_sentiment_value(submission.upvote_ratio)))
+            f.write(fm.mini_separator_1())
 
         f.close()
     
@@ -207,7 +214,15 @@ class SubredditSA:
             if isinstance(comment, MoreComments):
                 continue
             yield comment
+    
+    def _upvote_ratio_to_sentiment_value(self, upvote_ratio):
+        return 2 * upvote_ratio - 1
 
 
 if __name__ == '__main__':
-    pass
+    test = SubredditSA('chess')
+    test.title('hot', 2)
+    test.body('hot', 2)
+    test.top_comments('hot', 2)
+    test.sub_comments('hot', 2, 3)
+    test.votes('hot', 2)
