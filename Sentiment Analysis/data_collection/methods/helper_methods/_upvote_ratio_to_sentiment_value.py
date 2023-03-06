1c51bdc70a3831f38b7a154880fff35e8596d59d
Ryan Najjar <ryannajjar7@gmail.com>
2023-03-06 15:35:33 +0200

    delete Sentiment Analysis directory

--- Sentiment Analysis/data_collection/methods/helper_methods/_upvote_ratio_to_sentiment_value.py
+++
@@ -1,3 +0,0 @@
-def _upvote_ratio_to_sentiment_value(upvote_ratio):
-    return 2 * upvote_ratio - 1
-    
--- Sentiment Analysis/display_data/__init__.py
+++
@@ -1,5 +0,0 @@
-from display_data.methods.display_title_results import display_title_results
-from display_data.methods.display_body_results import display_body_results
-from display_data.methods.display_top_comments_results import display_top_comments_results
-from display_data.methods.display_sub_comments_results import display_sub_comments_results
-from display_data.methods.display_votes_results import display_votes_results
