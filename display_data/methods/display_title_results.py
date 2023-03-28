08afd12fce5a7993e2a634df0719530e619d27a7
Ryan Najjar <ryannajjar7@gmail.com>
2023-03-28 10:36:33 -0500

    refactor display_title_results function to allow presenting data in multiple forms

--- display_data/methods/display_title_results.py
+++ display_data/methods/display_title_results.py
@@ -2,17 +2,32 @@
 import display_data.methods.helper_methods.formatting as fm
 
 
-def display_title_results(collected_data):
+def display_title_results(collected_data, type, append=False):
     """Displays the data aquired from running the title() method."""
 
-    f = open('title_data.txt', 'w')
-    title_data = collected_data
+    if type == 'txt':
+        t = open('display_data/methods/templates/title_templates/title_display_template.txt', 'r')
+        d = open('title_data.txt', 'a')
+    elif type == 'md':
+        t = open('display_data/methods/templates/title_templates/title_display_template.md', 'r')
+        d = open('title_data.md', 'a')
+    elif type == 'raw':
+        f = open('title_data.txt', 'w')
+        f.write(str(collected_data))
+        f.close()
+        return
 
-    for data in title_data:
+    if not append:
+        d.truncate(0)
+    
+    for data in collected_data:
+        t.seek(0)
+        template = t.read()
         for title in data['titles']:
-            f.write(title[0] + '\n')
-            f.write('\n')
-            f.write(f'The title of this post has a sentiment of: {title[1]}\n')
-            f.write(fm.mini_separator_1())
-        
-    f.close()
+            template = template.replace('{title}', title[0])
+            template = template.replace('{sentiment}', str(title[1]))
+
+            d.write(template)
+    
+    t.close()
+    d.close()
---
+++ display_data/methods/templates/title_templates/title_display_template.md
@@ -0,0 +1,4 @@
+## {title}
+
+### The title of this post has a sentiment of: **{sentiment}**
+---
