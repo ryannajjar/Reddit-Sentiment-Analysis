d9cf7de3bf536e4b96dbc95162cedb7defd01fdc
Ryan Najjar <ryannajjar7@gmail.com>
2023-04-01 06:22:22 -0500

    add comments to display_body_results

--- display_data/methods/display_body_results.py
+++ display_data/methods/display_body_results.py
@@ -5,6 +5,7 @@ import display_data.methods.helper_methods.formatting as fm
 def display_body_results(collected_data, type, append=False):
     """Displays the data aquired from running the body() method."""
 
+    # check the type of file the user requested and open the appropriate files
     if type == 'txt':
         t = open('display_data/methods/templates/body_templates/body_display_template.txt', 'r')
         d = open('body_data.txt', 'a')
@@ -17,6 +18,7 @@ def display_body_results(collected_data, type, append=False):
         f.close()
         return
     
+    # reset the displaying file if append is still set to False
     if not append:
         d.truncate(0)
 
@@ -26,18 +28,22 @@ def display_body_results(collected_data, type, append=False):
         template = template.replace('{title}', data['title'])
 
         for body_info in data['content']:
+            # for data without a url and without a sentiment value
             if body_info[1] == '' and body_info[2] == '':
                 template = template.replace('{body}', body_info[0])
                 template = template.replace('{url}', '')
                 template = template.replace('{sentiment}', '')
+            # for data with a url and without a sentiment value
             elif body_info[1] == '':
                 template = template.replace('{body}', body_info[0])
                 template = template.replace('{url}', f'{fm.mini_separator_2()}\nUrl that the Reddit post links to: {body_info[2]}')
                 template = template.replace('{sentiment}', '')
+            # for data with a url and with a sentiment value
             elif body_info[1] != '' and body_info[2] != '':
                 template = template.replace('{body}', body_info[0])
                 template = template.replace('{url}', f'{fm.mini_separator_2()}\nUrl that the Reddit post links to: {body_info[2]}{fm.mini_separator_2()}')
                 template = template.replace('{sentiment}', f'The body of this post has a sentiment of: {body_info[1]}')
+            # for data without a url and with a snetiment value
             elif body_info[1] != '':
                 template = template.replace('{body}', body_info[0])
                 template = template.replace('{url}', '')
